10a733074bc3c274861f89c5c18646d376ab3d2f
LOHEZIC VICTOR <vlohezic@enseirb-matmeca.fr>
2023-12-11 16:08:30 +0100

    [feat] Create a unique key in output text

--- src/main/java/ClashRoyaleCleaning.java
+++ src/main/java/ClashRoyaleCleaning.java
@@ -6,68 +6,49 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 import java.util.*;
 
+
 public class ClashRoyaleCleaning {
-    public static IntWritable getCount(Iterable<IntWritable> values){
-        int count = 0;
-        for (IntWritable i: values) {
-            count += i.get();
-        }
-        return new IntWritable(count);
-    }
+
     public static class ClashRoyaleCleaningMapper
-            extends Mapper<Text, CityWritable, IntWritable, IntWritable> {
-        public static IntWritable count = new IntWritable(1);
-
+            extends Mapper<Object, Text, Text, IntWritable> {
         @Override
-        protected void map(Text key, CityWritable value, Context context) throws IOException, InterruptedException {
-            // Votre logique de mapping ici
-            context.write(new IntWritable(value.population), count);
-        }
-    }
-    public static class ClashRoyaleCleaningCombiner
-            extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
+        protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+            JSONObject game = null;
+            try {
+                game = new JSONObject(value.toString());
 
-        private static final ArrayList<IntWritable> intervals = new ArrayList<IntWritable>();
-
-        static {
-            intervals.add(new IntWritable(200000));
-            intervals.add(new IntWritable(400000));
-            intervals.add(new IntWritable(600000));
-            intervals.add(new IntWritable(800000));
-            intervals.add(new IntWritable(1000000));
-            intervals.add(new IntWritable(5000000));
-            intervals.add(new IntWritable(10000000));
-            intervals.add(new IntWritable(20000000));
-            intervals.add(new IntWritable(40000000));
-        }
-
-        public void reduce(IntWritable key, Iterable<IntWritable> values,Context context )
-                throws IOException, InterruptedException {
-            int i = 0;
-            while(key.compareTo(intervals.get(i)) > 0){
-                ++i;
+                String date = game.getString("date");
+                String round = game.getString("round");
+                String player1 = game.getString("player");
+                String player2 = game.getString("player2");
+                context.write(new Text(date+round+player1+player2),new IntWritable(0));
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
             }
-            context.write(intervals.get(i), getCount(values));
-        }
-    }
-    public static class ClashRoyaleCleaningReducer
-            extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
-        public void reduce(IntWritable key, Iterable<IntWritable> values,
-                           Context context
-        ) throws IOException, InterruptedException {
-            context.write(key, getCount(values));
         }
     }
 
-    public static class CityWritable implements Writable, Cloneable {
+    public static class ClashRoyaleCleaningReducer
+            extends Reducer<Text, IntWritable, Text, IntWritable> {
+        public void reduce(Text key, Iterable<IntWritable> values,Context context )
+                throws IOException, InterruptedException {
+            context.write(key, values.iterator().next());
+        }
+    }
+
+    /*
+    public static class GameWritable implements Writable, Cloneable {
         String country;
         String city;
         String accentCity;
@@ -76,13 +57,13 @@ public class ClashRoyaleCleaning {
         double latitude;
         double longitude;
 
-        CityWritable() {
+        GameWritable() {
 
         }
 
 
 
-        CityWritable(String country, String city, String accentCity, String region, int population, double latitude, double longitude) {
+        GameWritable(String country, String city, String accentCity, String region, int population, double latitude, double longitude) {
             this.country = country;
             this.city = city;
             this.accentCity = accentCity;
@@ -128,9 +109,9 @@ public class ClashRoyaleCleaning {
       }
 
         @Override
-        public CityWritable clone() {
+        public GameWritable clone() {
             try {
-                CityWritable clone = (CityWritable) super.clone();
+                GameWritable clone = (GameWritable) super.clone();
                 clone.population = this.population;
                 clone.country = this.country;
                 clone.region = this.region;
@@ -144,23 +125,20 @@ public class ClashRoyaleCleaning {
             }
         }
     }
-
+*/
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         Job job = Job.getInstance(conf, "ClashRoyaleCleaning");
         job.setNumReduceTasks(1);
         job.setJarByClass(ClashRoyaleCleaning.class);
         job.setMapperClass(ClashRoyaleCleaningMapper.class);
-        job.setMapOutputKeyClass(IntWritable.class);
+        job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(IntWritable.class);
-        job.setCombinerClass(ClashRoyaleCleaningCombiner.class);
-        job.setOutputKeyClass(IntWritable.class);
-        job.setOutputValueClass(IntWritable.class);
         job.setReducerClass(ClashRoyaleCleaningReducer.class);
-        job.setOutputKeyClass(IntWritable.class);
+        job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
         job.setOutputFormatClass(TextOutputFormat.class);
-        job.setInputFormatClass(SequenceFileInputFormat.class);
+        job.setInputFormatClass(TextInputFormat.class);
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
