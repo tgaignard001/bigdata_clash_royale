7e487e00b339dbb3bf2823828fd733ca6e49931e
LOHEZIC VICTOR <vlohezic@enseirb-matmeca.fr>
2023-12-23 13:37:26 +0100

    [fix] Refactor the function to send data from tree

--- src/main/java/ClashRoyaleTopK.java
+++ src/main/java/ClashRoyaleTopK.java
@@ -5,27 +5,58 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.TaskInputOutputContext;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class ClashRoyaleTopK {
+
+    private static void maintainTreeMapSize(TreeMap<Double, String> treeMap, int k){
+        while(treeMap.size() > k){
+            treeMap.remove(treeMap.firstKey());
+        }
+    }
+
+    private static void sendSummaryFromTreeMaps(TaskInputOutputContext<?,?, NullWritable, SummaryTopK> context, TreeMap<Double, String> winRateTopK, TreeMap<Double, String> meanDiffForceTopK) throws IOException, InterruptedException {
+        DeckTopK winRate = new DeckTopK();
+        DeckTopK meanDiffForce = new DeckTopK();
+        SummaryTopK output = new SummaryTopK();
+
+        Iterator<Map.Entry<Double, String>> winRateIterator = winRateTopK.entrySet().iterator();
+        Iterator<Map.Entry<Double, String>> meanDiffForceIterator = meanDiffForceTopK.entrySet().iterator();
+
+        while (winRateIterator.hasNext() && meanDiffForceIterator.hasNext()){
+            Map.Entry<Double, String> winRateEntry = winRateIterator.next();
+            Map.Entry<Double, String> meanDiffForceEntry = meanDiffForceIterator.next();
+
+            winRate.setId(winRateEntry.getValue());
+            winRate.setValue(winRateEntry.getKey());
+
+            meanDiffForce.setId(meanDiffForceEntry.getValue());
+            meanDiffForce.setValue(meanDiffForceEntry.getKey());
+
+            output.setValues(winRate.clone(), meanDiffForce.clone());
+            context.write(NullWritable.get(), output);
+        }
+    }
+
     public static class ClashRoyaleTopKMapper
             extends Mapper<Text, DeckSummaryWritable, NullWritable, SummaryTopK> {
 
         private final TreeMap<Double, String> winRateTopK = new TreeMap<Double, String>();
+        private final TreeMap<Double, String> meanDiffForceTopK = new TreeMap<Double, String>();
+
         private int k = 10;
 
         /**
          * Add in winRateTopk tree the winRate if the value is interesting
-         * @param deck
+         * @param id String with the cards of the deck
+         * @param deck DeckSummary with information of the deck
          */
         private void addWinRate(String id, DeckSummaryWritable deck){
             double victories = deck.getVictories();
@@ -34,6 +65,16 @@ public class ClashRoyaleTopK {
                 double winRate = victories/uses;
                 winRateTopK.put(winRate, id);
             }
+        }
+
+        /**
+         * Add in meanDiffForceTopk tree the mean difference force of the deck if the value is interesting
+         * @param id String with the cards of the deck
+         * @param deck DeckSummary with information of the deck
+         */
+        private void addMeanDiffForce(String id, DeckSummaryWritable deck){
+            double meanDiffForce = deck.getMeanDiffForce();
+            meanDiffForceTopK.put(meanDiffForce, id);
         }
 
         @Override
@@ -45,23 +86,15 @@ public class ClashRoyaleTopK {
         @Override
         protected void map(Text key, DeckSummaryWritable value, Context context) throws IOException, InterruptedException {
             addWinRate(key.toString(), value);
-            while(winRateTopK.size() > k){
-                winRateTopK.remove(winRateTopK.firstKey());
-            }
+            addMeanDiffForce(key.toString(), value);
+            maintainTreeMapSize(winRateTopK, k);
+            maintainTreeMapSize(meanDiffForceTopK, k);
         }
 
         @Override
         protected void cleanup(Mapper<Text, DeckSummaryWritable, NullWritable, SummaryTopK>.Context context)
                 throws IOException, InterruptedException {
-            DeckTopK winRate = new DeckTopK();
-            SummaryTopK output = new SummaryTopK();
-
-            for(Map.Entry<Double, String> pair : winRateTopK.entrySet()) {
-                winRate.setId(pair.getValue());
-                winRate.setValue(pair.getKey());
-                output.setWinRate(winRate.clone());
-                context.write(NullWritable.get(), output);
-            }
+            sendSummaryFromTreeMaps(context, winRateTopK, meanDiffForceTopK);
         }
     }
 
@@ -80,22 +113,16 @@ public class ClashRoyaleTopK {
         public void reduce(Text key, Iterable<SummaryTopK> values, Context context)
                 throws IOException, InterruptedException {
             TreeMap<Double, String> winRateTopK = new TreeMap<Double, String>();
+            TreeMap<Double, String> meanDiffForceTopK = new TreeMap<Double, String>();
+
             for (SummaryTopK value : values){
                 winRateTopK.put(value.getWinRate().getValue(), value.getWinRate().getId());
-                while(winRateTopK.size() > k) {
-                    winRateTopK.remove(winRateTopK.firstKey());
-                }
+                meanDiffForceTopK.put(value.getMeanDiffForce().getValue(), value.getMeanDiffForce().getId());
+                maintainTreeMapSize(winRateTopK, k);
+                maintainTreeMapSize(meanDiffForceTopK, k);
             }
 
-            DeckTopK winRate = new DeckTopK();
-            SummaryTopK output = new SummaryTopK();
-
-            for(Map.Entry<Double, String> pair : winRateTopK.entrySet()) {
-                winRate.setId(pair.getValue());
-                winRate.setValue(pair.getKey());
-                output.setWinRate(winRate.clone());
-                context.write(NullWritable.get(), output);
-            }
+            sendSummaryFromTreeMaps(context, winRateTopK, meanDiffForceTopK);
         }
     }
 
--- src/main/java/SummaryTopK.java
+++ src/main/java/SummaryTopK.java
@@ -1,33 +1,47 @@
 import org.apache.hadoop.io.Writable;
 
+import java.awt.*;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 
 public class SummaryTopK implements Writable, Cloneable {
     private DeckTopK winRate = new DeckTopK();
+    private DeckTopK meanDiffForce = new DeckTopK();
 
     public DeckTopK getWinRate() {
         return winRate;
     }
 
-    public void setWinRate(DeckTopK winRate) {
+    public DeckTopK getMeanDiffForce() {
+        return meanDiffForce;
+    }
+
+    public void setValues(DeckTopK winRate, DeckTopK meanDiffForce) {
         this.winRate = winRate;
+        this.meanDiffForce = meanDiffForce;
+    }
+
+    public void setMeanDiffForce(DeckTopK meanDiffForce) {
+        this.meanDiffForce = meanDiffForce;
     }
 
     @Override
     public void write(DataOutput out) throws IOException{
         winRate.write(out);
+        meanDiffForce.write(out);
     }
 
     @Override
     public void readFields(DataInput in) throws IOException{
         winRate.readFields(in);
+        meanDiffForce.readFields(in);
     }
 
     @Override
     public String toString() {
-        return "winRate: " + winRate;
+        return "winRate: " + winRate
+                + "meanDiffForce: " + meanDiffForce;
     }
 
     @Override
@@ -35,6 +49,7 @@ public class SummaryTopK implements Writable, Cloneable {
         try {
             SummaryTopK clone = (SummaryTopK) super.clone();
             clone.winRate = this.winRate.clone();
+            clone.meanDiffForce = this.meanDiffForce.clone();
             return clone;
         } catch (CloneNotSupportedException e) {
             throw new AssertionError();
